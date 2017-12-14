6a38e1dbf337d045f769969f3ee6772312c2bf69
unitrak-ricky <enrique.ogtip@itmildura.com.au>
2017-12-14 08:23:17 -0800

    small code update

--- RWBTaskWeb/src/app/channel.service.ts
+++ RWBTaskWeb/src/app/channel.service.ts
@@ -55,14 +55,9 @@ export class ChannelService {
     private startingSubject = new Subject<any>();
     private errorSubject = new Subject<any>();
     
-
-    // These are used to track the internal SignalR state 
-    //
     private hubConnection: any;
     private hubProxy: any;
 
-    // An internal array to track what channel subscriptions exist 
-    //
     private subjects = new Array<ChannelSubject>();
     private channelEvent = new ChannelEvent();
 
@@ -129,38 +124,6 @@ export class ChannelService {
             });
     }
 
-    /* sub(channel: string): Observable<ChannelEvent> {
-        
-        let channelSub = this.subjects.find((x: ChannelSubject) => {
-            return x.channel === channel;
-        }) as ChannelSubject;
-
-        if (channelSub !== undefined) {
-            console.log(`Found existing observable for ${channel} channel`)
-            return channelSub.subject.asObservable();
-        }
-        
-        channelSub = new ChannelSubject();
-        channelSub.channel = channel;
-        channelSub.subject = new Subject<ChannelEvent>();
-
-        this.subjects.push(channelSub);
-
-        this.starting$.subscribe(() => {
-            this.hubProxy.invoke("Subscribe", channel)
-                .done(() => {
-                    console.log(`Successfully subscribed to ${channel} channel`);
-                })
-                .fail((error: any) => {
-                    channelSub.subject.error(error);
-                });
-        },
-            (error: any) => {
-                channelSub.subject.error(error);
-            });
-
-        return channelSub.subject.asObservable();
-    } */
     
     publish(ev: ChannelEvent): void {
         this.hubProxy.invoke("Publish", ev);
